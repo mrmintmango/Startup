2ef2354da454afc3931f16340e85f7ff91d45728
mrmintmango <ruben.matos.imp@gmail.com>
2025-02-13 15:43:31 -0700

    fixed a title and it turns out that styling for basic html stuff is global

--- src/login/login.jsx
+++ src/login/login.jsx
@@ -5,7 +5,7 @@ import { NavLink } from 'react-router-dom';
 export function Login() {
   return (
     <main>
-    <h2>the Game Vault</h2>
+      <h2 className='titleWords'>the Game Vault</h2>
       
     </main>
   );
