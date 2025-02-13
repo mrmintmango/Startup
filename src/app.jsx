07e25f772928a72b89d2262ab08e43c102fdbfcd
mrmintmango <ruben.matos.imp@gmail.com>
2025-02-12 23:02:21 -0700

    added the routed components.

--- src/app.jsx
+++ src/app.jsx
@@ -14,13 +14,22 @@ export default function App() {
       <BrowserRouter>
         <div className="body bg-dark text-light">
             <ul class="navigation">
-                <li><a href="index.html">Logout</a></li>
-                <li><a href="vaultPage.html">Vault</a></li>
-                <li class="active"><a href="gameTime.html">GameTime</a></li>
-                <li><a href="friends.html">Friends</a></li>
+                <li><NavLink to=''>Logout</NavLink></li>
+                <li><NavLink to='vault'>Vault</NavLink></li>
+                <li><NavLink to='time'>GameTime</NavLink></li>
+                <li><NavLink to='friends'>Friends</NavLink></li>
             </ul>       
 
             <main>App componenets go here</main>
+
+            <Routes>
+            <Route path='/' element={<Login />} exact />
+            <Route path='/vault' element={<Vault />} />
+            <Route path='/time' element={<Time />} />
+            <Route path='/info' element={<Info />} />
+            <Route path='/friends' element={<Friends />} />
+            <Route path='*' element={<NotFound />} />
+            </Routes>
 
             <footer>
             <div></div>
@@ -30,4 +39,8 @@ export default function App() {
         </div>;
       </BrowserRouter>
     );
+  }
+
+  function NotFound() {
+    return <main className="container-fluid bg-secondary text-center">404: Return to sender. Address unknown.</main>;
   }
