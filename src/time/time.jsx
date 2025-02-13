426014575139ad4d2b6223ef4e0526b5e2d75d9e
mrmintmango <ruben.matos.imp@gmail.com>
2025-02-12 22:45:20 -0700

    Created the view components. They are currently just stubs

---
+++ src/time/time.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export function Time() {
+  return (
+    <main className="container-fluid bg-secondary text-center">
+      <div>game time displayed here</div>
+    </main>
+  );
+}
---
+++ src/vault/vault.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export function Vault() {
+  return (
+    <main className="container-fluid bg-secondary text-center">
+      <div>vault displayed here</div>
+    </main>
+  );
+}
