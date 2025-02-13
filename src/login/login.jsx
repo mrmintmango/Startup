45587419e4b2202a79d97631d7d2901399b04671
mrmintmango <ruben.matos.imp@gmail.com>
2025-02-13 16:21:16 -0700

    Good spot where login and friends are okay. many problems but okay.

--- src/login/login.jsx
+++ src/login/login.jsx
@@ -6,7 +6,20 @@ export function Login() {
   return (
     <main>
       <h2 className='titleWords'>the Game Vault</h2>
-      
+      <div className="wrapper">
+        <p className="login"> Login </p>
+        <li>
+          <label className='frontText' for="text">Username: </label>
+          <input className='inputBox' type="text" id="text" name="varText" placeholder="your name here" required pattern="[Aa].*" />
+        </li>
+        <li>
+          <label className='frontText' for="password">Password: </label>
+          <input className='inputBox' type="password" id="password" placeholder="your password here" name="varPassword" />
+        </li>
+        <div className='center'> 
+        <button className='buttons' type="button" onclick="location.href='vaultPage.html'">Register</button><button className='buttons' type="button" onclick="location.href='vaultPage.html'">Login</button><div></div>
+        </div>
+      </div> 
     </main>
   );
 }
