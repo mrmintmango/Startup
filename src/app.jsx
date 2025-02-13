dd1811039c9297352c2c615927bfd5e414eb1900
mrmintmango <ruben.matos.imp@gmail.com>
2025-02-12 21:35:43 -0700

    Set up the app component to display the header and footer correctly.

--- src/app.jsx
+++ src/app.jsx
@@ -3,5 +3,20 @@ import React from 'react';
 import './app.css';
 
 export default function App() {
-  return <div className="body bg-dark text-light">App will display here</div>;
+  return <div className="body bg-dark text-light">
+    <ul class="navigation">
+        <li><a href="index.html">Logout</a></li>
+        <li><a href="vaultPage.html">Vault</a></li>
+        <li class="active"><a href="gameTime.html">GameTime</a></li>
+        <li><a href="friends.html">Friends</a></li>
+    </ul>       
+
+    <main>App componenets go here</main>
+
+    <footer>
+    <div></div>
+    <p>created by Ruben Matos</p>
+    <a href="https://github.com/mrmintmango/Startup"> my Github! </a>
+    </footer>
+  </div>;
 }
