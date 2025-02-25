e6370d54f8beafca6312e84ea69399a3d18cbb5c
mrmintmango <ruben.matos.imp@gmail.com>
2025-02-25 16:25:26 -0700

    working on saving the username and password in local memory

--- src/login/login.jsx
+++ src/login/login.jsx
@@ -3,6 +3,16 @@ import './login.css';
 import { NavLink } from 'react-router-dom';
 
 export function Login() {
+  const [user, setUser] = React.useState('');
+  const [password, setPass] = React.useState('');
+
+  function userText(text) {
+    setUser(text.target.value);
+  }
+  function passText(text) {
+    setPass(text.target.value);
+  }
+
   return (
     <main className='mainLogin'>
       <h2 className='titleWords'>the Game Vault</h2>
@@ -10,14 +20,14 @@ export function Login() {
         <p className="login"> Login </p>
         <li>
           <label className='frontText' for="text">Username: </label>
-          <input className='inputBox' type="text" id="text" name="varText" placeholder="your name here" required pattern="[Aa].*" />
+          <input className='inputBox' type="text" placeholder="your name here" onChange={userText}/>
         </li>
         <li>
           <label className='frontText' for="password">Password: </label>
-          <input className='inputBox' type="password" id="password" placeholder="your password here" name="varPassword" />
+          <input className='inputBox' type="text" placeholder="your name here" onChange={passText}/>
         </li>
         <div className='center'> 
-        <button className='buttons' type="button" onclick="location.href='vaultPage.html'">Register</button><button className='buttons' type="button" onclick="location.href='vaultPage.html'">Login</button><div></div>
+        <button className='buttons' type="button">Register</button><button className='buttons' type="button">Login</button><div></div>
         </div>
       </div> 
     </main>
--- src/vault/info.jsx
+++ src/vault/info.jsx
@@ -1,7 +1,6 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import './gameInfoStyle.css';
 import { StarRating } from './StarRating';
-import { Vault } from './vault';
 import { useNavigate } from 'react-router-dom';
 
 export function Info() {
@@ -11,28 +10,39 @@ export function Info() {
   };
 
   const [rating, setRating] = useState(0);
+
   return (
     <main>
-      <button onClick={handleInfoClick}>Back to Vault</button><p className="gameInfoTitle">Game Title</p>
+      <button onClick={handleInfoClick}>Back to Vault</button>
+      <p className="gameInfoTitle">Game Title</p>
       <div className="grid-container">
-        <div className="item1"><p> Favorite? <input type="checkbox"/> </p></div>
-        <div className="item2"><img height={500} alt="gta" src="https://media.gamestop.com/i/gamestop/10161249?$pdp2x$"/></div>
+        <div className="item1">
+          <p> Favorite? 
+            <input type="checkbox" id="favorite" className="heart-checkbox" />
+            <label htmlFor="favorite" className="heart-label"></label>
+          </p>
+        </div>
+        <div className="item2">
+          <img height={500} alt="gta" src="https://media.gamestop.com/i/gamestop/10161249?$pdp2x$"/>
+        </div>
         <div className="item3">
           <p>Rating:</p>
           <StarRating rating={rating} onRatingChange={setRating} />
         </div>
-      <div className="item4">
-        <label for="textarea">Review: <br/></label>
-        <textarea id="textarea" name="varTextarea"></textarea>
-      </div>
-      <div className="item6">
-        <p> Memories: </p>
-        <div><input type="file" id="file" name="varFile" accept="image/*" multiple /></div>
-        <div className="memories-container">
+        <div className="item4">
+          <label htmlFor="textarea">Review: <br/></label>
+          <textarea id="textarea" name="varTextarea"></textarea>
+        </div>
+        <div className="item6">
+          <p> Memories: </p>
+          <div>
+            <input type="file" id="file" name="varFile" accept="image/*" multiple />
+          </div>
+          <div className="memories-container">
             <img height={300} alt="gta" src="https://media.gamestop.com/i/gamestop/10106497_10106499_10115457_10115461_10115462_10161249_10161250_SCR17/Grand-Theft-Auto-V---PlayStation-4?$screen2x$"/>
             <textarea id="textarea" name="varTextarea"></textarea>
+          </div>
         </div>
-      </div>
       </div>
       <div>
         <br/>
