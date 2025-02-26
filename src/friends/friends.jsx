ebd54df36c0e1b37891aea2d2aec36190dd85282
mrmintmango <ruben.matos.imp@gmail.com>
2025-02-25 17:11:24 -0700

    Now you can add more friends and their buttons go to the vault page!

--- src/friends/friends.jsx
+++ src/friends/friends.jsx
@@ -1,28 +1,57 @@
-import React from 'react';
-import './friendsStyle.css'
+import React, { useState } from 'react';
+import './friendsStyle.css';
+import { useNavigate } from 'react-router-dom';
 
 export function Friends() {
+  const navigate = useNavigate();
+  const [friends, setFriends] = useState([
+    { name: "Bob's Vault", imgSrc: 'https://ih1.redbubble.net/image.1082756148.9909/fposter,small,wall_texture,square_product,600x600.u1.jpg' },
+    { name: "Bob's Vault", imgSrc: 'https://ih1.redbubble.net/image.1082756148.9909/fposter,small,wall_texture,square_product,600x600.u1.jpg' },
+    { name: "Bob's Vault", imgSrc: 'https://ih1.redbubble.net/image.1082756148.9909/fposter,small,wall_texture,square_product,600x600.u1.jpg' },
+    { name: "Bob's Vault", imgSrc: 'https://ih1.redbubble.net/image.1082756148.9909/fposter,small,wall_texture,square_product,600x600.u1.jpg' },
+    { name: "Bob's Vault", imgSrc: 'https://ih1.redbubble.net/image.1082756148.9909/fposter,small,wall_texture,square_product,600x600.u1.jpg' },
+    { name: "Bob's Vault", imgSrc: 'https://ih1.redbubble.net/image.1082756148.9909/fposter,small,wall_texture,square_product,600x600.u1.jpg' },
+    { name: "Bob's Vault", imgSrc: 'https://ih1.redbubble.net/image.1082756148.9909/fposter,small,wall_texture,square_product,600x600.u1.jpg' },
+    { name: "Bob's Vault", imgSrc: 'https://ih1.redbubble.net/image.1082756148.9909/fposter,small,wall_texture,square_product,600x600.u1.jpg' },
+    { name: "Bob's Vault", imgSrc: 'https://ih1.redbubble.net/image.1082756148.9909/fposter,small,wall_texture,square_product,600x600.u1.jpg' },
+    { name: "Bob's Vault", imgSrc: 'https://ih1.redbubble.net/image.1082756148.9909/fposter,small,wall_texture,square_product,600x600.u1.jpg' },
+  ]);
+  const [newFriend, setNewFriend] = useState('');
+
+  const handleVaultClick = () => {
+    navigate('/vault');
+  };
+
+  const handleNewFriendChange = (event) => {
+    setNewFriend(event.target.value);
+  };
+
+  const handleAddFriend = () => {
+    if (newFriend.trim() !== '') {
+      setFriends([...friends, { name: newFriend, imgSrc: 'https://via.placeholder.com/150' }]);
+      setNewFriend('');
+    }
+  };
+
   return (
     <main className="friendmain">
       <div>
         <h3>Check out your friends vaults!</h3>
-            <div className='scrollmenu'>
-                <button><img src="https://ih1.redbubble.net/image.1082756148.9909/fposter,small,wall_texture,square_product,600x600.u1.jpg" /><br/>Bob's Vault</button>
-                <button><img src="https://ih1.redbubble.net/image.1082756148.9909/fposter,small,wall_texture,square_product,600x600.u1.jpg" /><br/>Bob's Vault</button>
-                <button><img src="https://ih1.redbubble.net/image.1082756148.9909/fposter,small,wall_texture,square_product,600x600.u1.jpg" /><br/>Bob's Vault</button>
-                <button><img src="https://ih1.redbubble.net/image.1082756148.9909/fposter,small,wall_texture,square_product,600x600.u1.jpg" /><br/>Bob's Vault</button>
-                <button><img src="https://ih1.redbubble.net/image.1082756148.9909/fposter,small,wall_texture,square_product,600x600.u1.jpg" /><br/>Bob's Vault</button>
-                <button><img src="https://ih1.redbubble.net/image.1082756148.9909/fposter,small,wall_texture,square_product,600x600.u1.jpg" /><br/>Bob's Vault</button>
-                <button><img src="https://ih1.redbubble.net/image.1082756148.9909/fposter,small,wall_texture,square_product,600x600.u1.jpg" /><br/>Bob's Vault</button>
-                <button><img src="https://ih1.redbubble.net/image.1082756148.9909/fposter,small,wall_texture,square_product,600x600.u1.jpg" /><br/>Bob's Vault</button>
-                <button><img src="https://ih1.redbubble.net/image.1082756148.9909/fposter,small,wall_texture,square_product,600x600.u1.jpg" /><br/>Bob's Vault</button>
-                <button><img src="https://ih1.redbubble.net/image.1082756148.9909/fposter,small,wall_texture,square_product,600x600.u1.jpg" /><br/>Bob's Vault</button>
-            </div>
+        <div className='scrollmenu'>
+          {friends.map((friend, index) => (
+            <button key={index} onClick={handleVaultClick}>
+              <img src={friend.imgSrc} alt={friend.name} /><br />{friend.name}
+            </button>
+          ))}
+        </div>
 
-            <div>
-            <p>add Friend:</p>
-            <div class="center"> <input type="search" id="search" name="varSearch" /><button type="button">Add</button></div>
-            </div>
+        <div>
+          <p>add Friend:</p>
+          <div className="center">
+            <input type="search" id="search" name="varSearch" value={newFriend} onChange={handleNewFriendChange} />
+            <button type="button" onClick={handleAddFriend}>Add</button>
+          </div>
+        </div>
       </div>
     </main>
   );
