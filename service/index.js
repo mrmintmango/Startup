d09ac5cc8397a8f3d72318f1ea0a879e5cee81aa
mrmintmango <ruben.matos.imp@gmail.com>
2025-03-07 15:07:59 -0700

    Copied over from the simon instruction page and got the node mudules and packages insive the service directory. Also added things for login and logout for the index.js file which is funny because it reminded me to make a logout functionality.

--- service/index.js
+++ service/index.js
@@ -1,12 +1,84 @@
+const cookieParser = require('cookie-parser');
+const bcrypt = require('bcrypt');
+const uuid = require('uuid');
+
 const express = require('express');
 const app = express();
 
+app.use(express.json());
+
+let users = [];
+let vaults = [];
+
+let apiRouter = express.Router();
+app.use('/api', apiRouter);
+
 const port = process.argv.length > 2 ? process.argv[2] : 3000;
 
-app.get('*', (_req, res) => {
-  res.send({ msg: 'Hello Backend!' });
+// app.listen(port, () => {
+//   console.log(`Backend is listening on port ${port}`);
+// });
+
+// CreateAuth a new user
+apiRouter.post('/auth/create', async (req, res) => {
+  if (await findUser('email', req.body.email)) {
+    res.status(409).send({ msg: 'Existing user' });
+  } else {
+    const user = await createUser(req.body.email, req.body.password);
+
+    setAuthCookie(res, user.token);
+    res.send({ email: user.email });
+  }
 });
 
-app.listen(port, () => {
-  console.log(`Backend is listening on port ${port}`);
+// GetAuth login an existing user
+apiRouter.post('/auth/login', async (req, res) => {
+  const user = await findUser('email', req.body.email);
+  if (user) {
+    if (await bcrypt.compare(req.body.password, user.password)) {
+      user.token = uuid.v4();
+      setAuthCookie(res, user.token);
+      res.send({ email: user.email });
+      return;
+    }
+  }
+  res.status(401).send({ msg: 'Unauthorized' });
+});
+
+// DeleteAuth logout a user
+apiRouter.delete('/auth/logout', async (req, res) => {
+  const user = await findUser('token', req.cookies[authCookieName]);
+  if (user) {
+    delete user.token;
+  }
+  res.clearCookie(authCookieName);
+  res.status(204).end();
+});
+
+// Middleware to verify that the user is authorized to call an endpoint
+const verifyAuth = async (req, res, next) => {
+  const user = await findUser('token', req.cookies[authCookieName]);
+  if (user) {
+    next();
+  } else {
+    res.status(401).send({ msg: 'Unauthorized' });
+  }
+};
+
+//GetVault gets the vault for the current user
+
+//GetPic gets a pic from the third party api for the button images
+
+//PutVault updates the vault for the current user
+
+
+
+// Default error handler
+app.use(function (err, req, res, next) {
+  res.status(500).send({ type: err.name, message: err.message });
+});
+
+// Return the application's default page if the path is unknown
+app.use((_req, res) => {
+  res.sendFile('index.html', { root: 'public' });
 });
