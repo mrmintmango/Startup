831cb4b8c1514356a9e91b0e5a1e43a08d4a2fd6
mrmintmango <ruben.matos.imp@gmail.com>
2025-03-22 09:18:50 -0600

    Pasted over some mongo code from the dataServices section on github. Still need to set it up with my code.

--- service/index.js
+++ service/index.js
@@ -5,6 +5,16 @@ const express = require('express');
 const fs = require('fs');
 const path = require('path');
 
+const { MongoClient } = require('mongodb');
+const config = require('./dbConfig.json');
+
+const url = `mongodb+srv://${config.userName}:${config.password}@${config.hostname}`;
+
+const client = new MongoClient(url);
+const db = client.db('rental');
+const collection = db.collection('house');
+
+
 const app = express();
 
 app.use(express.json());
